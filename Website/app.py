117ecf50bed3a813527e3e4793858aa5fc915886
Praneet9 <praneetbomma@gmail.com>
2018-12-22 11:04:54 +0530

    sensor data visualization done

--- Website/app.py
+++ Website/app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, jsonify, request
 from static import db
+from random import sample, randint
 app = Flask(__name__)
 
 db.testingdb()
+
+def frange(start, stop, step):
+    i = start
+    while i < stop:
+        yield i
+        i += step
 
 @app.route("/test")
 def index():
@@ -24,7 +31,7 @@ def index():
         print("t1",var)
         # var = request.args['h2']
         # print("h2",var)
-        # var = request.args['t2']
+        # var = request.args['t2']vi
         # print("t2",var)
     except Exception as e:
         print(e)
@@ -36,6 +43,19 @@ def test():
     print("SMS2",request.args['x'])
     return "Hello"
 
+@app.route("/data")
+def data():
+    labels = []
+    for i in range(48):
+        labels.append(randint(0, 100))
+    print(labels)
+    d = {
+        'results': sample(labels, 48),
+        'plot_labels': list(frange(0.5, 24.5, 0.5))
+        }
+
+    return jsonify(d)
+
 @app.route("/chart")
 def chart():
     d = {'left': [12, 19, 3, 5, 2, 30, 15, 15, 6, 20], 'right': 0.82339555, '_unknown_': 0.0059609693}
@@ -45,7 +65,10 @@ def chart():
     #     'scores': d
     # }
     # resp = jsonify(message)
-    return render_template('index.html', name = d )
+    return render_template('index.html') 
+
+
+    
 
 
 
--- Website/static/js/main.js
+++ Website/static/js/main.js
@@ -1,81 +1,61 @@
-// var ctx = document.getElementById("myChart1").getContext('2d');
-// var myChart = new Chart(ctx, {
-//     type: 'line',
-//     data: {
-//         labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-//         datasets: [{
-//             label: 'Soil Moisture Sensor',
-//             data: [12, 19, 3, 5, 2, 3, 15, 6, 20],
-//             backgroundColor: [
-//                 'transparent',
-//             ],
-//             borderColor: [
-//                'blue'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero:true
-//                 }
-//             }]
-//         }
-//     }
-// });
-// var ctx = document.getElementById("myChart2").getContext('2d');
-// var myChart = new Chart(ctx, {
-//     type: 'line',
-//     data: {
-//         labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-//         datasets: [{
-//             label: 'Soil Moisture Sensor',
-//             data: [12, 19, 3, 5, 2, 3, 15, 6, 20],
-//             backgroundColor: [
-//                 'transparent',
-//             ],
-//             borderColor: [
-//                'blue'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero:true
-//                 }
-//             }]
-//         }
-//     }
-// });
-// var ctx = document.getElementById("myChart3").getContext('2d');
-// var myChart = new Chart(ctx, {
-//     type: 'line',
-//     data: {
-//         labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-//         datasets: [{
-//             label: 'Soil Moisture Sensor',
-//             data: [12, 19, 3, 5, 2, 3, 15, 6, 20],
-//             backgroundColor: [
-//                 'transparent',
-//             ],
-//             borderColor: [
-//                'blue'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero:true
-//                 }
-//             }]
-//         }
-//     }
-// });
+jQuery.get('/data').done(function(results) {
+        plot(results);
+});
+
+function plot(data) {
+    var plant1 = document.getElementById("plant1").getContext('2d');
+    var plant2 = document.getElementById("plant2").getContext('2d');
+    var plant3 = document.getElementById("plant3").getContext('2d');
+    var plant4 = document.getElementById("plant4").getContext('2d');
+    var plant5 = document.getElementById("plant5").getContext('2d');
+    var plant6 = document.getElementById("plant6").getContext('2d');
+
+    var temp1 = document.getElementById("temp1").getContext('2d');
+    var temp2 = document.getElementById("temp2").getContext('2d');
+
+    var humidity1 = document.getElementById("humidity1").getContext('2d');
+    var humidity2 = document.getElementById("humidity2").getContext('2d');
+
+    myChart(plant1, data.results, data.plot_labels, 'Plant 1');
+    myChart(plant2, data.results, data.plot_labels, 'Plant 2');
+    myChart(plant3, data.results, data.plot_labels, 'Plant 3');
+    myChart(plant4, data.results, data.plot_labels, 'Plant 4');
+    myChart(plant5, data.results, data.plot_labels, 'Plant 5');
+    myChart(plant6, data.results, data.plot_labels, 'Plant 6');
+
+    myChart(temp1, data.results, data.plot_labels, 'Temperature 1');
+    myChart(temp2, data.results, data.plot_labels, 'Temperature 2');
+
+    myChart(humidity1, data.results, data.plot_labels, 'Humidity 1');
+    myChart(humidity2, data.results, data.plot_labels, 'Humidity 2');
+
+}
+
+function myChart(context, plot_data, plot_labels, label) {
+    new Chart(context, {
+        type: 'line',
+        data: {
+            labels: plot_labels,
+            datasets: [{
+                label: label,
+                data: plot_data,
+                backgroundColor: [
+                    '#26A69A'
+                ],
+                borderColor: [
+                   'green'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+}
