9b1654bcf8d24b67898e23b167014b9d64363100
Praneet9 <praneetbomma@gmail.com>
2019-04-06 09:17:49 +0530

    file upload ajax done

--- Website/app.py
+++ Website/app.py
@@ -1,24 +1,28 @@
 from flask import Flask, render_template, jsonify, request
 from bson import json_util
-import datetime
+from datetime import datetime
 import pytz
 import math
 from keras.models import model_from_json
 import tensorflow as tf
 import cv2
 import numpy as np
+import os
 
-global model, graph, label_dictionary
+if not os.path.exists('uploads'):
+    os.mkdir('uploads')
 
-label_dictionary = {0: 'Early Blight', 1: 'Healthy', 2: 'Late Blight'}
+# global model, graph, label_dictionary
 
-json_file = open('model/model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-model = model_from_json(loaded_model_json)
-model.load_weights("model/model.h5")
+# label_dictionary = {0: 'Early Blight', 1: 'Healthy', 2: 'Late Blight'}
 
-graph = tf.get_default_graph()
+# json_file = open('model/model.json', 'r')
+# loaded_model_json = json_file.read()
+# json_file.close()
+# model = model_from_json(loaded_model_json)
+# model.load_weights("model/model.h5")
+
+# graph = tf.get_default_graph()
 
 app = Flask(__name__)
 
@@ -28,22 +32,23 @@ def index():
 
 @app.route("/image", methods = ['POST'])
 def predict():
-    test = request.files['file-field']
-    print(test)
-    test.save('test.jpg')
+    test = request.files['file']
+    path = str(datetime.now())
+    test.save('uploads/' + path + '.jpg')
 
-    image_path = "test.jpg"
+    # image_path = "test.jpg"
 
-    img = cv2.imread(image_path)
-    output = cv2.resize(img, (256, 256)).copy()
-    img = cv2.resize(img, (128, 128))
-    img = img / 255
-    with graph.as_default():
-        proba = model.predict(img.reshape(-1, 128, 128, 3))
+    # img = cv2.imread(image_path)
+    # output = cv2.resize(img, (256, 256)).copy()
+    # img = cv2.resize(img, (128, 128))
+    # img = img / 255
+    # with graph.as_default():
+    #     proba = model.predict(img.reshape(-1, 128, 128, 3))
 
-    idx = np.argmax(proba)
-    label = '<h1>' + label_dictionary[idx] + " ====> " + str(np.max(proba) * 100)[:5] + "%" + '</h1>'
-    return label
+    # idx = np.argmax(proba)
+    # label = '<h1>' + label_dictionary[idx] + " ====> " + str(np.max(proba) * 100)[:5] + "%" + '</h1>'
+    # return label
+    return "tjankluo"
 
 if __name__ == '__main__':
     app.run(debug=False)
