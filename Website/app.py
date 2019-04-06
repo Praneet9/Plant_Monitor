3e7fd7a6f4e69d960c856ddc2160d9df7ee4a9d4
Praneet9 <praneetbomma@gmail.com>
2019-04-06 17:58:35 +0530

    final website done

--- Website/app.py
+++ Website/app.py
@@ -1,14 +1,11 @@
 from flask import Flask, render_template, jsonify, request
 from werkzeug.utils import secure_filename
-from bson import json_util
 from datetime import datetime
 from predict_health import predict_svm, predict_rfc, predict_kmeans
-import pytz
-import math
-# from keras.models import model_from_json
-# import tensorflow as tf
-# import cv2
-# import numpy as np
+from keras.models import model_from_json
+import tensorflow as tf
+import cv2
+import numpy as np
 import os
 
 if not os.path.exists('uploads'):
@@ -16,30 +13,35 @@ if not os.path.exists('uploads'):
 
 ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg', 'JPG', 'JPEG']
 
-# global model, graph, label_dictionary
+global model, graph, label_dictionary
 
-# label_dictionary = {0: 'Early Blight', 1: 'Healthy', 2: 'Late Blight'}
+label_dictionary = {0: 'Early Blight', 1: 'Healthy', 2: 'Late Blight'}
 
-# json_file = open('model/model.json', 'r')
-# loaded_model_json = json_file.read()
-# json_file.close()
-# model = model_from_json(loaded_model_json)
-# model.load_weights("model/model.h5")
+json_file = open('model/model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+model.load_weights("model/model.h5")
 
-# graph = tf.get_default_graph()
+graph = tf.get_default_graph()
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/image", methods = ['POST'])
+
+@app.route("/image", methods=['POST'])
 def predict():
     image = request.files['file']
     extension = secure_filename(image.filename).split('.')[-1]
     if extension not in ALLOWED_EXTENSIONS:
-        return jsonify({'status':False, 'message':'Image type not supported'})
+        return jsonify({
+            'status': False,
+            'message': 'Image type not supported'
+        })
     else:
         image_path = 'uploads/' + str(datetime.now()) + '.' + extension
         try:
@@ -52,12 +54,20 @@ def predict():
             with graph.as_default():
                 proba = model.predict(img.reshape(-1, 128, 128, 3))
         except Exception as _:
-            return jsonify({'status':False, 'message': 'Something went wrong!'})
+            return jsonify({
+                'status': False,
+                'message': 'Something went wrong!'
+            })
 
         idx = np.argmax(proba)
-        return jsonify({'status':True, 'message': label_dictionary[idx], 'score': str(np.max(proba) * 100)[:5] + "%"})
+        return jsonify({
+            'status': True,
+            'message': label_dictionary[idx],
+            'score': str(np.max(proba) * 100)[:5] + "%"
+        })
 
-@app.route("/api", methods = ['POST'])
+
+@app.route("/api", methods=['POST'])
 def prediction():
     test = request.files['photo']
     print(test)
@@ -76,21 +86,34 @@ def prediction():
 
     res = label_dictionary[idx]
     cc = str(np.max(proba) * 100)[:5]
-    print(cc)
     #label = '<h1>' + label_dictionary[idx] + " ====> " + str(np.max(proba) * 100)[:5] + "%" + '</h1>'
-    return jsonify({'status':res, 'cc':cc})
+    return jsonify({'status': res, 'cc': cc})
+
 
 # Sensors Part
 @app.route("/sensor", methods=['POST'])
 def health_prediction():
-    soil_moisture = request.form['soil_moisture']
-    humidity = request.form['humidity']
-    temperature = request.form['temperature']
+    data = request.form.to_dict()
+
+    soil_moisture = float(data['soil_moisture'])
+    humidity = float(data['humidity'])
+    temperature = float(data['temperature'])
+
+    if soil_moisture < 0 or soil_moisture > 100 or humidity < 0 or humidity > 100 or temperature < 0 or temperature > 100:
+        return jsonify({'status': False, 'message': 'Incorrect Sensor Data!'})
+
     svm_result = predict_svm(soil_moisture, humidity, temperature)
     rfc_result = predict_rfc(soil_moisture, humidity, temperature)
     kmeans_result = predict_kmeans(soil_moisture, humidity, temperature)
+    
+    return jsonify({
+        'status': True,
+        'message': 'Success',
+        'kmeans': kmeans_result,
+        'svm': svm_result,
+        'rfc': rfc_result
+    })
 
-    return "Hello World"
 
 if __name__ == '__main__':
     app.run(debug=False)
--- Website/static/js/main.js
+++ Website/static/js/main.js
@@ -9,40 +9,83 @@ function previewImage(event){
     }
     reader.readAsDataURL(event.target.files[0]);
     console.log("Starting");
-        $("#submit").click(function(){
-    
-            var fd = new FormData();
-            var files = $('#file-field')[0].files[0];
-            fd.append('file',files);
-            
-            $('#disorder').css('display', 'none');
-            $('#score').css('display', 'none');
-            $('#predtext').css('display', 'none');
-            $('#loader').css('display', 'inline');
-
-            $.ajax({
-                url: '/image',
-                type: 'post',
-                data: fd,
-                mimeType:"multipart/form-data",
-                contentType: false,
-                timeout: 6000,
-                processData: false,
-                success: function(response){
-                    var result = JSON.parse(response);
-                    console.log(result);
-                    if(result.status){
-                        $('#loader').css('display', 'none');
-                        $('#disorder').text("Disorder: " + result.message);
-                        $('#disorder').css('display', 'inline');
-                        $('#score').text("Confidence Score: " + result.score);
-                        $('#score').css('display', 'inline');
-                    }else{
-                        $('#disorder').text(result.message);
-                        $('#disorder').css('color', 'red');
-                    }
-                },
-            });
-        });
-
 }
+
+$("#image_upload").click(function(){
+    
+    var fd = new FormData();
+    var files = $('#file-field')[0].files[0];
+    fd.append('file',files);
+    
+    $('#disorder').css('display', 'none');
+    $('#score').css('display', 'none');
+    $('#predtext_image').css('display', 'none');
+    $('#loader_image').css('display', 'inline');
+
+    $.ajax({
+        url: '/image',
+        type: 'post',
+        data: fd,
+        mimeType:"multipart/form-data",
+        contentType: false,
+        timeout: 6000,
+        processData: false,
+        success: function(response){
+            var result = JSON.parse(response);
+            console.log(result);
+            if(result.status){
+                $('#loader_image').css('display', 'none');
+                $('#disorder').text("Disorder: " + result.message);
+                $('#disorder').css('display', 'inline');
+                $('#score').text("Confidence Score: " + result.score);
+                $('#score').css('display', 'inline');
+            }else{
+                $('#loader_image').css('display', 'none');
+                $('#disorder').text(result.message);
+                $('#disorder').css('color', 'red');
+            }
+        },
+    });
+});
+
+$("#sensor_data").click(function(){
+    
+    $('#rfc').css('display', 'none');
+    $('#kmeans').css('display', 'none');
+    $('#svm').css('display', 'none');
+    $('#predtext_sensor').css('display', 'none');
+    $('#loader_sensor').css('display', 'inline');
+
+    var temperature = $('#temperature').val();
+    var humidity = $('#humidity').val();
+    var soil_moisture = $('#soil_moisture').val();
+
+    var sensors = {"temperature": temperature, "humidity": humidity, "soil_moisture": soil_moisture};
+
+    $.ajax({
+        url: '/sensor',
+        type: 'post',
+        data: sensors,
+        timeout: 6000,
+        success: function(response){
+            // var result = JSON.parse(response);
+            console.log(response);
+            result = response
+            if(result.status){
+                $('#loader_sensor').css('display', 'none');
+
+                $('#rfc').css('display', 'inline');
+                $('#kmeans').css('display', 'inline');
+                $('#svm').css('display', 'inline');
+
+                $('#rfc').text("RFC: " + result.rfc);
+                $('#kmeans').text("KMeans: " + result.kmeans);
+                $('#svm').text("SVM: " + result.svm);
+            }else{
+                $('#loader_sensor').css('display', 'none');
+                $('#kmeans').text(result.message);
+                $('#kmeans').css('color', 'red');
+            }
+        },
+    });
+});
